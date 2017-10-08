2b96699e9cc437b5b87323ca10d82c334862c8a4
WojciechBakun <wojciechbakun@gmail.com>
2017-10-08 19:10:22 +0200

    zad 1 i 2 w jednym
    zadania 1 i 2 rozdzileczone switchem

--- main.cpp
+++ main.cpp
@@ -17,13 +17,16 @@ void gotoxy( int x, int y )
 
 int main()
 {
-    int x=40, y=1, wiersze=10, gwiazdki=1;
+    int x=40, y=6, wiersze=10, gwiazdki=1, trojkat;
     cout<<"Podaj liczbe wierszy trojkata: ";
     cin>>wiersze;
-
+    cout<<endl<<"Podaj typ trojkata \n [1] - normalny \n [2] - z odstepami \n Twoj wybor: ";
+    cin>>trojkat;
     gotoxy(x,y);
 
-    for(int i=0; i<wiersze; i++){
+    switch (trojkat){
+    case 1:
+        for(int i=0; i<wiersze; i++){
         gotoxy(x,y);
         for(int i = 0; i < gwiazdki; i++)
         {
@@ -32,5 +35,25 @@ int main()
         gwiazdki=gwiazdki+2;
         x--;
         y++;
+        }
+        break;
+    case 2:
+        for(int i=0; i<wiersze; i++){
+        gotoxy(x,y);
+        for(int i = 0; i < gwiazdki; i++)
+        {
+            if(i%2==0){
+                cout<<"*";
+            } else
+                cout<<" ";
+        }
+        gwiazdki=gwiazdki+2;
+        x--;
+        y++;
+        }
+        break;
+    default:
+        cout<<"Nie ma takiego wyboru, prawidlowe opcje to [1] lub [2]!";
+        break;
     }
 }
